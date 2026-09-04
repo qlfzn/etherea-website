1ae949a1562189c2ba3bbfa0d906cbf338a44472
qlfzn <aqilfauzan09@gmail.com>
2026-09-04 22:39:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Etherea - Order System Website
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <section id='hero' className='flex flex-col min-h-screen justify-center items-center'>
+      <h1
+        id='header'
+        className='text-2xl'
+      >
+        Etherea
+      </h1>
+      <p id='brand-description' className='text-gray-500'>
+        Fragrances for everyone.
+      </p>
+      <button id='order-cta' onClick='' className='mt-4'>Order</button>
+    </section>
+    </>
+  )
+}
+
+export default App
